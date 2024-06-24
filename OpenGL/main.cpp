f5e663d0d39e271d3ed14dbd7779c25e74520b7c
WindyFeg <phongspeed7@yahoo.com.vn>
2024-06-24 11:41:16 +0700

    Create Triangle and Move it

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -1,12 +1,145 @@
 // Normal C++ Libs
 #include<stdio.h>
+#include <string.h>
+#include <cmath>
 
 // Opengl Libs
-#include<GL/glew.h>
-#include<GLFW/glfw3.h>
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
+
+// Math Libary
+#include <glm\glm.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+#include <glm\gtc\type_ptr.hpp>
 
 //Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
+
+GLuint VBO, VAO, shader, uniformModel;
+
+bool direction = true;
+float triOffset = 0.0f;
+float triMaxOffset = 0.7f;
+float triIncrement = 0.0005f;
+
+// Vertex Shader code
+static const char* vShader = "                                                \n\
+#version 330                                                                  \n\
+                                                                              \n\
+layout (location = 0) in vec3 pos;											  \n\
+                                                                              \n\
+uniform mat4 model;                                                          \n\
+                                                                              \n\
+void main()                                                                   \n\
+{                                                                             \n\
+    gl_Position = model * vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);				  \n\
+}";
+
+// Fragment Shader
+static const char* fShader = "                                                \n\
+#version 330                                                                  \n\
+                                                                              \n\
+out vec4 colour;                                                               \n\
+                                                                              \n\
+void main()                                                                   \n\
+{                                                                             \n\
+    colour = vec4(1.0, 0.0, 0.0, 1.0);                                         \n\
+}";
+
+
+void CreateTriangle()
+{
+	GLfloat vertices[] = {
+		-1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f
+	};
+
+	// Generate and bind a Vertex Array Object (VAO)
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	// Generate and bind a Vertex Buffer Object (VBO)
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// Introduce the vertices data into the buffer
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// Define the layout of the vertex data
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	// Enable the vertex attribute
+	glEnableVertexAttribArray(0);
+
+	// Unbind the VBO
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	// Unbind the VAO
+	glBindVertexArray(0);
+}
+
+void AddShader(GLuint theProgram, const char* shaderCode, GLenum shaderType)
+{
+	GLuint theShader = glCreateShader(shaderType);
+
+	const GLchar* theCode[1];
+	theCode[0] = shaderCode;
+
+	GLint codeLength[1];
+	codeLength[0] = strlen(shaderCode);
+
+	glShaderSource(theShader, 1, theCode, codeLength);
+	glCompileShader(theShader);
+
+	GLint result = 0;
+	GLchar eLog[1024] = { 0 };
+
+	glGetShaderiv(theShader, GL_COMPILE_STATUS, &result);
+	if (!result) 
+	{
+		glGetShaderInfoLog(theShader, 1024, NULL, eLog);
+		fprintf(stderr, "Error compiling the %d shader: '%s'\n", shaderType, eLog);
+		return;
+	}
+
+	glAttachShader(theProgram, theShader);
+}
+
+
+void CompileShaders()
+{
+	shader = glCreateProgram();
+
+	if (!shader) 
+	{
+		printf("Failed to create shader\n");
+		return;
+	}
+
+	AddShader(shader, vShader, GL_VERTEX_SHADER);
+	AddShader(shader, fShader, GL_FRAGMENT_SHADER);
+
+	GLint result = 0;
+	GLchar eLog[1024] = { 0 };
+
+	glLinkProgram(shader);
+	glGetProgramiv(shader, GL_LINK_STATUS, &result);
+	if (!result) 
+	{
+		glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+		printf("Error linking program: '%s'\n", eLog);
+		return;
+	}
+
+	glValidateProgram(shader);
+	glGetProgramiv(shader, GL_VALIDATE_STATUS, &result);
+	if (!result) 
+	{
+		glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+		printf("Error validating program: '%s'\n", eLog);
+		return;
+	}
+
+	uniformModel = glGetUniformLocation(shader, "model");
+}
 
 int main() {
 	//Initialise GLFW
@@ -65,17 +198,50 @@ int main() {
 	//Setup Viewport size
 	glViewport(0, 0, bufferWidth, bufferHeight);
 
-	//Loop until window closed
+	CreateTriangle();
+	CompileShaders();
+
+
+	// Loop until window closed
 	while (!glfwWindowShouldClose(mainWindow))
 	{
-		//Get + Handle user input events
+		// Get + Handle user input events
 		glfwPollEvents();
 
-		//Clear window
-		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+		if (direction)
+		{
+			triOffset += triIncrement;
+		}
+		else {
+			triOffset -= triIncrement;
+		}
+
+		if (abs(triOffset) >= triMaxOffset)
+		{
+			direction = !direction;
+		}
+
+		// Clear window
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shader);
+
+		glm::mat4 model(1.0f);
+		model = glm::translate(model, glm::vec3(triOffset, triOffset, 0.0f));
+
+
+		// glUniform1f(uniformModel, triOffset);
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
+
+		glUseProgram(0);
 
 		glfwSwapBuffers(mainWindow);
 	}
+
 	return 0;
 }
